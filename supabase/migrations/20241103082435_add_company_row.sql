02a94241cff685da45ddb12178fb23467e851b5c
yoheisuda <y1405705@gmail.com>
2024-11-03 17:33:48 +0900

    change table

---
+++ supabase/migrations/20241103082435_add_company_row.sql
@@ -0,0 +1,2 @@
+ALTER TABLE employees
+ADD COLUMN company VARCHAR(255);
--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,5 +1,5 @@
 -- テストデータを挿入
-insert into employees (name, email) values
-('山田 太郎', 'taro.yamada@example.com'),
-('鈴木 花子', 'hanako.suzuki@example.com'),
-('佐藤 次郎', 'jiro.sato@example.com');
+insert into employees (name, email, company) values
+('山田 太郎', 'taro.yamada@example.com', '株式会社山田'),
+('鈴木 花子', 'hanako.suzuki@example.com', '株式会社鈴木'),
+('佐藤 次郎', 'jiro.sato@example.com', '株式会社佐藤');
