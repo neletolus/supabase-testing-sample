75d9c8770329ede487dadabe2ce0170c1cc95aed
yoheisuda <y1405705@gmail.com>
2024-11-03 17:52:46 +0900

    change table

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,5 +1,5 @@
 -- テストデータを挿入
-insert into employees (name, email, company) values
-('山田 太郎', 'taro.yamada@example.com', '株式会社山田'),
-('鈴木 花子', 'hanako.suzuki@example.com', '株式会社鈴木'),
-('佐藤 次郎', 'jiro.sato@example.com', '株式会社佐藤');
+insert into employees (email) values
+('taro.yamada@example.com'),
+('hanako.suzuki@example.com'),
+('jiro.sato@example.com');
