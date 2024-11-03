ce7af2373aaf42de767cf3459170ccc9af05ac11
yoheisuda <y1405705@gmail.com>
2024-11-03 17:05:54 +0900

    init

---
+++ supabase/seed.sql
@@ -0,0 +1,5 @@
+-- テストデータを挿入
+insert into employees (name, email) values
+('山田 太郎', 'taro.yamada@example.com'),
+('鈴木 花子', 'hanako.suzuki@example.com'),
+('佐藤 次郎', 'jiro.sato@example.com');
---
+++ supabase/tests/database/employees.test.sql
@@ -0,0 +1,34 @@
+begin;
+select plan(6);
+
+SELECT has_column(
+    'public',
+    'employees',
+    'id',
+    'id should exist'
+);
+SELECT has_column(
+    'public',
+    'employees',
+    'name',
+    'name should exist'
+);
+SELECT has_column(
+    'public',
+    'employees',
+    'email',
+    'email should exist'
+);
+SELECT has_column(
+    'public',
+    'employees',
+    'created_at',
+    'created_at should exist'
+);
+
+SELECT has_pk('public', 'employees', 'id');
+
+SELECT col_has_default('employees', 'created_at');
+
+select * from finish();
+rollback;
